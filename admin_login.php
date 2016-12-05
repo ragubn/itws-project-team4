e549e46b380e50913c3d424197bd4035272796fa
ragubn <natasharagubir23@gmail.com>
2016-12-05 11:38:11 -0500

    adding comments

--- admin_login.php
+++ admin_login.php
@@ -8,7 +8,7 @@ session_destroy();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> <!-- setting title, linking to APIs, bootstrap, and styles -->
   <title>Rensselaer Research Explorer</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,6 +21,7 @@ session_destroy();
 </head>
 <body>
 
+<!-- navigation bar, using unordered list for page options-->
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -74,9 +75,10 @@ session_destroy();
       </div>
     </form>
   </div>
-  <!-- end:Main Form -->
+  <!-- end of the Main Form -->
 </div>
 
+<!-- footer same for all pages -->
 <footer class="container-fluid text-center">
   <p>RPI 2016</p>
 </footer>
